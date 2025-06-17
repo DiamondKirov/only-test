92f01ea4021246cfce1befc24f5d4f43abe16d95
diamondkirov <diamondkirov@gmail.com>
2025-06-17 07:49:46 +0300

    init

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import styles from "./index.module.scss";
+
+const App = () => {
+  return <></>;
+};
+
+const root = createRoot(document.getElementById("root")!);
+root.render(<App />);
